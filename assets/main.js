cea459e05777da81d8a9aaab0228c24058c342cf
abeerbaf <abeer.bafagih@starthmore.edu>
2022-06-22 16:21:10 +0300

    My commit

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,27 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
+
+//button ten code
+const button10 = document.querySelector("#plusTen")
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+
+//button 15 code
+const button15 = document.querySelector("#plusFifteen")
+
+button15.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+//button 20 code
+const button20 = document.querySelector("#plusTwenty")
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+})
